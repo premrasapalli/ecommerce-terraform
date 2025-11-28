854a736db28dbe3c7ee4fc4852288a538069c49d
premrasapalli <premrasapalli82@gmail.com>
2025-11-28 19:02:45 +0530

    error image removed

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions = jsonencode([
     {
       name = "web"
-      image = "${var.repo_url}:\${IMAGE_TAG}"
+      image = "${var.repo_url}:${var.image_tag}"
       essential = true
       portMappings = [{ containerPort = 80, hostPort = 80, protocol = "tcp" }]
       logConfiguration = {
