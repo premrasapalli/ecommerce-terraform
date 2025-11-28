739323e94483440dd2f241d57ba6678a28a68f7d
premrasapalli <premrasapalli82@gmail.com>
2025-11-28 23:01:49 +0530

    changed the configs

--- infra/modules/alb/variable.tf
+++ infra/modules/alb/variable.tf
@@ -1,20 +1,50 @@
+#########################
+# ALB Module Variables
+#########################
+
 variable "vpc_id" {
-  description = "VPC ID for ALB"
+  description = "VPC ID where ALB will be deployed"
   type        = string
 }
 
 variable "public_subnets" {
-  description = "List of public subnets for ALB"
+  description = "List of public subnets for the ALB"
   type        = list(string)
 }
 
 variable "domain_name" {
-  description = "Domain name for ALB listener rule"
+  description = "Optional custom domain name"
   type        = string
+  default     = ""
 }
 
 variable "certificate_arn" {
-  description = "ACM certificate ARN for HTTPS listener"
+  description = "Optional ACM certificate ARN for HTTPS"
+  type        = string
+  default     = ""
+}
+
+variable "enable_https" {
+  description = "Enable HTTPS listener"
+  type        = bool
+  default     = false
+}
+
+variable "project_name" {
+  description = "Project name for tagging"
   type        = string
 }
+
+variable "tags" {
+  description = "Additional tags to apply"
+  type        = map(string)
+  default     = {}
+}
+
+variable "container_port" {
+  description = "Port used by ECS tasks behind the ALB"
+  type        = number
+  default     = 80
+}
+
 
