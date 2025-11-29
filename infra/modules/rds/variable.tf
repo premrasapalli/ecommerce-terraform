08c73c64c7508415496874c6adf05bc499443e04
premrasapalli <premrasapalli82@gmail.com>
2025-11-29 21:50:19 +0530

    changed the configs

--- infra/modules/rds/variable.tf
+++ infra/modules/rds/variable.tf
@@ -14,4 +14,19 @@ variable "tags" {
   type = map(string)
   default = {}
 }
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
 
